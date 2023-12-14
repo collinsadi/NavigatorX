c950af183592cf79c7f78d8e65b989a66fd858b4
collinsadi <collinsadi20@gmail.com>
2023-12-14 15:34:32 -0800

    fetch url changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors())
 // cron to keep server online (runs every 5 minutes)
 cron.schedule("*/10 * * * *", () => {
     try {
-      axios.get("/starter")
+      axios.get("https://rsunx.onrender.com/starter")
     } catch (e) {
       console.log(e.message);
     }
