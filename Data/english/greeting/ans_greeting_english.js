5ac0a039837b0b6791708a9fa234c1c9137617dc
collinsadi <collinsadi20@gmail.com>
2023-12-27 21:31:15 -0800

    Greeting Intent Modified

--- Data/english/greeting/ans_greeting_english.js
+++ Data/english/greeting/ans_greeting_english.js
@@ -2,7 +2,127 @@ const intent = "greeting_english";
 
 const answer_greeting_english = [
     {
-        answer:"Good Day, How may I help You Today?",
+        answer: "Good Day, How may I help You Today?",
+        intent
+    },
+    {
+        answer: "Hello! What can I assist you with?",
+        intent
+    },
+    {
+        answer: "Hi there! How can I be of service?",
+        intent
+    },
+    {
+        answer: "Hey! What brings you here today?",
+        intent
+    },
+    {
+        answer: "Good Day! How can I assist you this fine day?",
+        intent
+    },
+    {
+        answer: "Good Day! What can I do for you?",
+        intent
+    },
+    {
+        answer: "Good Day! How may I help you at this hour?",
+        intent
+    },
+    {
+        answer: "Howdy! What can I do for you?",
+        intent
+    },
+    {
+        answer: "What's up? How can I assist you today?",
+        intent
+    },
+    {
+        answer: "Greetings! What brings you to me?",
+        intent
+    },
+    {
+        answer: "Yo! How can I be of help?",
+        intent
+    },
+    {
+        answer: "How's it going? What can I do for you?",
+        intent
+    },
+    {
+        answer: "Nice to meet you! How may I assist you?",
+        intent
+    },
+    {
+        answer: "What's cracking? How can I help?",
+        intent
+    },
+    {
+        answer: "Salutations! What brings you here?",
+        intent
+    },
+    {
+        answer: "Good to see you! How may I assist you today?",
+        intent
+    },
+    {
+        answer: "How do you do? What can I help you with?",
+        intent
+    },
+    {
+        answer: "Aloha! How can I assist you today?",
+        intent
+    },
+    {
+        answer: "Sup! What brings you to me?",
+        intent
+    },
+    {
+        answer: "How are things? How can I assist you?",
+        intent
+    },
+    {
+        answer: "Pleasure to be here! What can I do for you?",
+        intent
+    },
+    {
+        answer: "What's happening? How may I assist you today?",
+        intent
+    },
+    {
+        answer: "Nice day, isn't it? How can I be of service?",
+        intent
+    },
+    {
+        answer: "What's good? How may I help you today?",
+        intent
+    },
+    {
+        answer: "Morning! What brings you to me?",
+        intent
+    },
+    {
+        answer: "Good Day! How can I assist you?",
+        intent
+    },
+    {
+        answer: "Good Day! What brings you here?",
+        intent
+    },
+    {
+        answer: "How's your day? How can I assist you?",
+        intent
+    },
+    {
+        answer: "How's everything? What can I do for you?",
+        intent
+    },
+    {
+        answer: "How's life? How may I help you today?",
+        intent
+    },
+    {
+        answer: "Good to see you! What brings you to me?",
         intent
     }
 ];
--- Data/english/greeting/greeting_english.js
+++ Data/english/greeting/greeting_english.js
@@ -2,10 +2,129 @@ const intent = "greeting_english";
 
 const greeting_english = [
     {
-        prompt:"how are you doing?",
+        prompt: "how are you doing?",
+        intent
+    },
+    {
+        prompt: "hello",
+        intent
+    },
+    {
+        prompt: "hi",
+        intent
+    },
+    {
+        prompt: "hey",
+        intent
+    },
+    {
+        prompt: "good morning",
+        intent
+    },
+    {
+        prompt: "good afternoon",
+        intent
+    },
+    {
+        prompt: "good evening",
+        intent
+    },
+    {
+        prompt: "howdy",
+        intent
+    },
+    {
+        prompt: "what's up?",
+        intent
+    },
+    {
+        prompt: "greetings",
+        intent
+    },
+    {
+        prompt: "yo",
+        intent
+    },
+    {
+        prompt: "how's it going?",
+        intent
+    },
+    {
+        prompt: "nice to meet you",
+        intent
+    },
+    {
+        prompt: "what's cracking?",
+        intent
+    },
+    {
+        prompt: "salutations",
+        intent
+    },
+    {
+        prompt: "good to see you",
+        intent
+    },
+    {
+        prompt: "how do you do?",
+        intent
+    },
+    {
+        prompt: "aloha",
+        intent
+    },
+    {
+        prompt: "sup",
+        intent
+    },
+    {
+        prompt: "how are things?",
+        intent
+    },
+    {
+        prompt: "pleasure to be here",
+        intent
+    },
+    {
+        prompt: "what's happening?",
+        intent
+    },
+    {
+        prompt: "nice day, isn't it?",
+        intent
+    },
+    {
+        prompt: "what's good?",
+        intent
+    },
+    {
+        prompt: "morning",
+        intent
+    },
+    {
+        prompt: "afternoon",
+        intent
+    },
+    {
+        prompt: "evening",
+        intent
+    },
+    {
+        prompt: "how's your day?",
+        intent
+    },
+    {
+        prompt: "how's everything?",
+        intent
+    },
+    {
+        prompt: "how's life?",
+        intent
+    },
+    {
+        prompt: "good to see you",
         intent
     }
 ];
-
 
 module.exports = greeting_english;
