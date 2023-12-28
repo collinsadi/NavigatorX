a233ec4ebfc9917933303ed5b07c8c233de59d8a
collinsadi <collinsadi20@gmail.com>
2023-12-28 09:51:57 -0800

    Cors Origin Changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use(cors({
-    origin:"https://rsuchat.netlify.app"
+    origin:"https://rsux.netlify.app"
 }))
 app.use(routes)
 
